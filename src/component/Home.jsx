ce1d34cfaf9c54df9a9e0e9f43e5d2dd140abc8e
Ganesh Kusumbe <gkusumbe7@gmail.com>
2024-07-27 23:25:41 +0530

    added code

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,6 @@ import { FaLocationDot } from "react-icons/fa6";
 const Home = () => {
   const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState(null);
-  // const ]\
   const [currentCityWeather, setCurrentCityWeather] = useState(null);
   const [cityOneweatherData, setCityOneweatherData] = useState(null);
   const [cityTwoweatherData, setCityTwoweatherData] = useState(null);
@@ -95,13 +94,14 @@ const Home = () => {
   console.log(weatherData);
   return (
     <>
-      <section className="flex flex-col gap-0">
+    <div className="bg-gradient-to-r from-teal-400 to-gray-800 min-h-screen">
+    <section className="flex flex-col bg-gradient-to-r ">
         <section className="flex flex-col items-center">
-          <div className="  ">
+          <div>
             <Input
               type="email"
               placeholder="Search City"
-              className="mt-5 !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-600 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
+              className="mt-5  !border !border-gray-300 bg-white text-gray-900 shadow-lg shadow-gray-900/5 ring-4 ring-transparent placeholder:text-gray-600 placeholder:opacity-100 focus:!border-gray-900 focus:!border-t-gray-900 focus:ring-gray-900/10"
               labelProps={{
                 className: "hidden",
               }}
@@ -110,36 +110,36 @@ const Home = () => {
               onChange={onChangeCityName}
             />
           </div>
-          <div className="mt-8 w-fit px-40">
+          <div className="mt-8  w-fit px-40">
             <Button onClick={searchCityWeather}>Get Search</Button>;
           </div>
         </section>
         <section>
-          <div className="flex flex-col justify-center m-5 p-10  items-center shadow-xl bg-yellow-300  rounded-xl">
+          <div className="flex flex-col  md:w-1/2 md:ml-80 justify-center m-2 p-4  items-center shadow-xl bg-gradient-to-t from-teal-400 to-yellow-300 rounded-xl">
             {weatherData ? (
               <>
-                <h1 className="flex gap-2 items-center mb-5 font-light text-blue-gray-50 text-xl">
+                <h1 className="flex gap-2 items-center mb-5 font-light text-black text-xl">
                   <FaLocationDot /> {weatherData.name}
                 </h1>
-                <p className="mb-2 text-white font-bold text-4xl">
-                  {Math.round(currentCityWeather.main.temp - 273.15)}°C
+                <p className="mb-2 text-blue-gray-800 font-bold text-4xl">
+                  {Math.round(weatherData.main.temp - 273.15)}°C
                 </p>
 
                 <img
                   src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
-                  className="mb-3 w-18 bg-deep-orange-50 shadow-xl  rounded-xl p-2"
+                  className="mb-4 w-18  shadow-2xl border  rounded-xl p-2"
                   alt="weather icon"
                 />
-                <p className="font-semibold text-white text-xl">
-                  {weatherData.weather[0].description}
+                <p className="mt-2 font-semibold text-white text-[18px]">
+                  {weatherData.weather[0].description.toUpperCase()}
                 </p>
               </>
             ) : currentCityWeather ? (
               <>
-                <h1 className="flex gap-2 items-center mb-5 font-light text-blue-gray-900 text-xl">
+                <h1 className="flex gap-2 items-center mb-5 font-light text-black text-xl">
                   <FaLocationDot /> {currentCityWeather.name}
                 </h1>
-                <p className="mb-2 text-blue-gray-500 font-bold text-4xl">
+                <p className="mb-2 text-blue-gray-800 font-bold text-4xl">
                   {Math.round(currentCityWeather.main.temp - 273.15)}°C
                 </p>
 
@@ -148,20 +148,21 @@ const Home = () => {
                   className="mb-3 w-18 shadow-2xl  rounded-xl p-2"
                   alt="weather icon"
                 />
-                <p className="font-semibold text-white text-xl">
-                  {currentCityWeather.weather[0].description}
+                <p className="mt-2 font-semibold text-white text-[18px]">
+                  {currentCityWeather.weather[0].description.toUpperCase()}
                 </p>
               </>
             ) : (
-              <div>
+              <div className="md:w-1/2  text-center" >
                 <h1>Loading your current city weather...</h1>
               </div>
             )}
           </div>
         </section>
-        <section className="flex">
+        <section className="flex justify-evenly mt-0">
             {cityOneweatherData && (
-              <div className="m-2 flex flex-col items-center gap-1 shadow-lg bg-blue-100 w-1/3 p-5 rounded-xl">
+              <div className="m-2 md:w-60 md:p-12 md:gap-5
+               flex flex-col items-center gap-2  shadow-lg bg-gradient-to-t from-indigo-200 to-cyan-100 w-1/3  rounded-xl">
                 <h1 className="flex gap-1 items-center font-bold text-blue-gray-800">
                 <FaLocationDot/>{cityOneweatherData.name}
                 </h1>
@@ -176,7 +177,7 @@ const Home = () => {
             )}
 
             {cityTwoweatherData && (
-              <div className="m-2 flex flex-col items-center gap-1 shadow-lg bg-blue-100 w-1/3 p-5 rounded-xl">
+              <div className="m-2 md:w-60 md:p-12 md:gap-5 flex flex-col items-center gap-1 shadow-lg bg-gradient-to-t from-indigo-200 to-cyan-100 w-1/3 p-5 rounded-xl">
                 <h1 className="flex gap-1 items-center font-bold text-blue-gray-800">
                 <FaLocationDot/>{cityTwoweatherData.name}
                 </h1>
@@ -191,7 +192,7 @@ const Home = () => {
             )}
 
             {cityThireeweatherData && (
-              <div className="m-2 flex flex-col items-center gap-1 shadow-lg bg-blue-100 w-1/3 p-5 rounded-xl">
+              <div className="m-2 md:w-60 md:p-14 md:gap-3 flex flex-col items-center gap-3 shadow-lg bg-gradient-to-t from-indigo-200 to-cyan-100 w-1/3 p-5 rounded-xl">
                 <h1 className="flex gap-1 items-center font-bold text-blue-gray-800">
                 <FaLocationDot/>{cityThireeweatherData.name}
                 </h1>
@@ -206,7 +207,8 @@ const Home = () => {
             )}
           </section>
       </section>
-    </>
+    </div>
+          </>
   );
 };
 
